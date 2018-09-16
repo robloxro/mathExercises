0e233b43fa8fe2a3227a103d1900a8abc7d01128
ruxij <ruxi.jitianu@gmail.com>
2018-09-16 11:53:25 +0300

    Exercise A - 4th grade math = Given xy+yz+zx=<given number> find all possible values for the number xyz

---
+++ src/robloxro/mathExercises/ExerciseA.java
@@ -0,0 +1,39 @@
+package robloxro.mathExercises;
+/*
+Given xy+yz+zx=<given number> find all possible values for the number xyz
+* */
+
+import static java.lang.System.exit;
+import static java.lang.System.in;
+
+public class ExerciseA {
+    public static void main(String[] args) {
+        int inputNumber;
+        if ((args.length<1) || (args[0]==null))
+        {
+            System.out.println("Please introduce an integer as result of xy+yz+zx\n");
+            return;
+        }else
+        {
+            try{
+                inputNumber = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println(args[0]+" is not a number.Please introduce an integer as result of xy+yz+zx\n");
+                return;
+            }
+        }
+        System.out.println("Number provided is "+inputNumber+"\n");
+        boolean found=false;
+        for (int xy=10; xy<100;xy++)
+            for (int yz=10; yz<100;yz++)
+                for (int zx=10; zx<100;zx++)
+                {
+                    if ((xy/10==zx%10) && (xy%10==yz/10) && (yz%10==zx/10) && (xy+yz+zx==inputNumber))
+                    {
+                        System.out.println("xy="+xy+",yz="+yz+",zx="+zx+"\n");
+                        found=true;
+                    }
+                }
+        System.out.println(found?"We found the combination":"There is no combination to match the input number "+inputNumber);
+    }
+}
